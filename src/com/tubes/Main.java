e4977db2d17c9229f7065652d79dcf70a83dcef5
KevinSenjaya72 <kevinsenjaya72@gmail.com>
2020-12-28 09:50:50 +0700

    Add Login Controller

--- src/com/tubes/Main.java
+++ src/com/tubes/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("View/MainLayout.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View/LoginLayout.fxml"));
         stage.setTitle("Login Tubes");
         stage.setScene(new Scene(root));
         stage.show();
