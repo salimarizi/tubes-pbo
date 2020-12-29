7b60ea9b68e2d33c8a560fd08f67ef0353335df8
Salim Arizi <salim9asmp1pdl@gmail.com>
2020-12-29 10:49:16 +0700

    FXML Spareparts

---
+++ src/com/tubes/Controller/Spareparts/FormSparepartController.java
@@ -0,0 +1,19 @@
+package com.tubes.Controller.Spareparts;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
+public class FormSparepartController {
+    public JFXTextField txtNama;
+    public JFXTextField txtQuantity;
+    public JFXTextField txtHarga;
+    public JFXButton btnSubmit;
+    public JFXButton btnCancel;
+
+    public void submitData(ActionEvent actionEvent) {
+    }
+
+    public void cancelProcess(ActionEvent actionEvent) {
+    }
+}
---
+++ src/com/tubes/Controller/Spareparts/SparepartController.java
@@ -0,0 +1,27 @@
+package com.tubes.Controller.Spareparts;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+
+public class SparepartController {
+    public JFXButton btnService;
+    public JFXButton btnSparepart;
+    public JFXButton btnUser;
+    public JFXButton btnLogout;
+    public JFXButton btnAddData;
+    public JFXButton btnEditData;
+    public JFXButton btnDeleteData;
+    public TableColumn clName;
+    public TableColumn clQuantity;
+    public TableColumn clPrice;
+
+    public void addUser(ActionEvent actionEvent) {
+    }
+
+    public void editUser(ActionEvent actionEvent) {
+    }
+
+    public void deleteUser(ActionEvent actionEvent) {
+    }
+}
